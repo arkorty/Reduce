c54ea8cd1418b0d5e4bf63b25d3bc3ee17f26f12
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-08-06 19:56:51 +0530

    Fix: redirect only 6 letter alpha-numerics IDs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/:id",
+        source: "/:id([a-zA-Z0-9]{6})", // Match only 6-character IDs
         destination: `/api/redirect/:id`,
         permanent: false,
       },
